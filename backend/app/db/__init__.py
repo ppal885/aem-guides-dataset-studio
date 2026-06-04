4f8f83ec77ddca57848157bfd99d9d20d5fde87c
prashantp <prashantp@adobe.com>
2026-06-05 00:49:40 +0530

    fixing import issues

--- backend/app/db/__init__.py
+++ backend/app/db/__init__.py
@@ -1,5 +1,9 @@
 from . import base, session, migrations
 from .base import Base
-from .session import Session, db_session
+from .session import Session, db_session, engine, SessionLocal, DATABASE_URL
 
-__all__ = ["base", "session", "migrations", "Base", "Session", "db_session"]
+__all__ = [
+    "base", "session", "migrations",
+    "Base",
+    "Session", "db_session", "engine", "SessionLocal", "DATABASE_URL",
+]
